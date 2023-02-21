3daede585f70aa8c90eab22bb864929cc5182c25
Kyour.cn <kyour@vip.qq.com>
2023-02-21 11:18:48 +0800

    [perf] 命令行优化

--- core/helps.go
+++ core/helps.go
@@ -3,14 +3,12 @@ package core
 const NoCmdHelp = `Welcome To Go-Gourd Command Console!
 Usage: %s COMMAND
 Commands:
-  start    Start gourd application
-  stop     Stop gourd application
+  start    Start gourd app.
 `
 
 const UndefinedHelp = `The command '%s' is not exists!
 Welcome To Go-Gourd Command Console!
 Usage: %s COMMAND
 Commands:
-  start    Start gourd application
-  stop     Stop gourd application
+  start    Start gourd app.
 `
