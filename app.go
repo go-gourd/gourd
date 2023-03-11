2da2ac4e1e42d9984888e327926fb282a12fa561
Kyour.cn <kyour@vip.qq.com>
2023-03-11 15:44:07 +0800

    [perf] v0.2.2 version

--- app.go
+++ app.go
@@ -25,8 +25,8 @@ type App struct {
 // Init 初始化应用
 func (app *App) Init() {
 
-	app.Version = 3
-	app.VersionName = "0.2.1"
+	app.Version = 4
+	app.VersionName = "0.2.2"
 	app.Conf = config.GetAppConfig()
 	app.TempDir = app.Conf.TempDir
 
