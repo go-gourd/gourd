8cd1828f5dea9b65c0520be6d2c5b8f3104f6495
Kyour <kyour@outlook.com>
2025-04-07 13:24:09 +0800

    style: 代码格式化调整

--- test/event_test.go
+++ test/event_test.go
@@ -1,48 +1,48 @@
 package test
 
 import (
-	"context"
-	"fmt"
-	"github.com/go-gourd/gourd/event"
-	"testing"
+    "context"
+    "fmt"
+    "github.com/go-gourd/gourd/event"
+    "testing"
 )
 
 func TestEvent(t *testing.T) {
 
-	// register event listener
-	event.Listen("test.add", func(ctx context.Context) {
-		fmt.Println("test.add", ctx)
-	})
+    // register event listener
+    event.Listen("test.add", func(ctx context.Context) {
+        fmt.Println("test.add", ctx)
+    })
 
-	event.Listen("test.edit", func(ctx context.Context) {
-		fmt.Println("test.edit", ctx)
-	})
+    event.Listen("test.edit", func(ctx context.Context) {
+        fmt.Println("test.edit", ctx)
+    })
 
-	event.Listen("user.add", func(ctx context.Context) {
-		fmt.Println("user.add", ctx)
-	})
+    event.Listen("user.add", func(ctx context.Context) {
+        fmt.Println("user.add", ctx)
+    })
 
-	event.Listen("user.edit", func(ctx context.Context) {
-		fmt.Println("user.edit", ctx)
-	})
+    event.Listen("user.edit", func(ctx context.Context) {
+        fmt.Println("user.edit", ctx)
+    })
 
-	event.Listen("user.address.add", func(ctx context.Context) {
-		value := ctx.Value("test_key")
-		fmt.Println("user.address.add", value)
-	})
+    event.Listen("user.address.add", func(ctx context.Context) {
+        value := ctx.Value("test_key")
+        fmt.Println("user.address.add", value)
+    })
 
-	// trigger event
-	ctx := context.WithValue(context.Background(), "test_key", "test_value")
+    // trigger event
+    ctx := context.WithValue(context.Background(), "test_key", "test_value")
 
-	fmt.Println("---------test.*-----------")
-	event.Trigger("test.*", ctx)
+    fmt.Println("---------test.*-----------")
+    event.Trigger("test.*", ctx)
 
-	fmt.Println("---------*.add-----------")
-	event.Trigger("*.add", ctx)
+    fmt.Println("---------*.add-----------")
+    event.Trigger("*.add", ctx)
 
-	fmt.Println("---------user.address.add-----------")
-	event.Trigger("user.address.add", ctx)
+    fmt.Println("---------user.address.add-----------")
+    event.Trigger("user.address.add", ctx)
 
-	fmt.Println("---------user.*.add-----------")
-	event.Trigger("user.*.add", ctx)
+    fmt.Println("---------user.*.add-----------")
+    event.Trigger("user.*.add", ctx)
 }
